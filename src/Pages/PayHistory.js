7e6429836e43b29748f926a7e7cb907662aaa6cb
Soap171 <spiyumal48@gmail.com>
2023-11-26 18:04:03 +0530

    add payhistory

--- src/Pages/PayHistory.js
+++ src/Pages/PayHistory.js
@@ -7,7 +7,7 @@ import supabase from '../config/supabaseClient';
 
 function PayHistory() {
   const [payHistoryData, setPayHistoryData] = useState([]);
-  const [searchEmail, setSearchEmail] = useState('');
+  
 
   useEffect(() => {
     // Fetch data from the 'Payment History' table in Supabase
@@ -35,32 +35,12 @@ function PayHistory() {
   }, []); // Ensure the effect runs only once
 
   
-  const handleSearch = (event) => {
-    setSearchEmail(event.target.value);
-  };
-
-
-  const filteredData = payHistoryData.filter((payment) => {
-    // Filter by email
-    return payment['Email'].toLowerCase().includes(searchEmail.toLowerCase());
-  });
-
   return (
     <>
       <Navbar />
       <Hero cName="hero-other" heroImg={payHistoryImg} title="Pay History Of Customers" />
       <div className="container mt-4 mb-4">
-        <div className="row">
-          <div className="col-md-6 mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search by Email"
-            value={searchEmail}
-            onChange={handleSearch}
-         />
-          </div>
-        </div>
+       
         <div className="row">
           <div className="col-md-12">
             <div className="table-responsive">
@@ -71,23 +51,23 @@ function PayHistory() {
                     <th>Value</th>
                     <th>Payment Type</th>
                     <th>Payment Date</th>
-                    <th>Balance</th>
                     <th>Booking ID</th>
-                    <th>Email</th>
+                    {/* Add other table header as needed */}
                   </tr>
                 </thead>
                 <tbody>
-                  {filteredData.map((payment, index) => (
-                    <tr key={index}>
-                      <td>{payment['Payment ID']}</td>
-                      <td>${payment.Value}</td>
-                      <td>{payment['Payment Type']}</td>
-                      <td>{payment['Payment Date']}</td>
-                      <td>{payment['Booking ID']}</td>
-                      <td>{payment.Email}</td>
-                      {/* Add other table data as needed */}
-                    </tr>
-                  ))}
+                  {payHistoryData
+                   
+                    .map((payment, index) => (
+                      <tr key={index}>
+                        <td>{payment.PaymentID}</td>
+                        <td>${payment.Value}</td>
+                        <td>{payment.PaymentType}</td>
+                        <td>{payment.PaymentDate}</td>
+                        <td>{payment.BookingID}</td>
+                        {/* Add other table data as needed */}
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
