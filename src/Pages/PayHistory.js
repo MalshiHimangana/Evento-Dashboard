8f335284025ea87f44996bc949ffbed0a2ffeba0
Soap171 <spiyumal48@gmail.com>
2023-11-26 18:10:05 +0530

    added the search option for payment history

--- src/Pages/PayHistory.js
+++ src/Pages/PayHistory.js
@@ -7,17 +7,17 @@ import supabase from '../config/supabaseClient';
 
 function PayHistory() {
   const [payHistoryData, setPayHistoryData] = useState([]);
-  
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Fetch data from the 'Payment History' table in Supabase
+   
     const fetchPayHistory = async () => {
       try {
         const { data, error } = await supabase
           .from('PaymentHistory')
           .select('*')
-          .order('PaymentDate', { ascending: false }) // Order by Payment Date, latest first
-          .limit(10); // Limit the results to 10 records
+          .order('PaymentDate', { ascending: false }) 
+          .limit(10); 
 
         if (error) {
           throw error;
@@ -32,15 +32,34 @@ function PayHistory() {
     };
 
     fetchPayHistory();
-  }, []); // Ensure the effect runs only once
+  }, []); 
 
-  
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredData = payHistoryData.filter((payment) => {
+    // Filter by the last 4 digits of PaymentID
+    const lastFourDigits = payment.PaymentID.slice(-4);
+    return lastFourDigits.includes(searchTerm);
+  });
+
   return (
     <>
       <Navbar />
       <Hero cName="hero-other" heroImg={payHistoryImg} title="Pay History Of Customers" />
       <div className="container mt-4 mb-4">
-       
+        <div className="row">
+          <div className="col-md-6 mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by Last 4 Digits of PaymentID"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+          </div>
+        </div>
         <div className="row">
           <div className="col-md-12">
             <div className="table-responsive">
@@ -52,22 +71,18 @@ function PayHistory() {
                     <th>Payment Type</th>
                     <th>Payment Date</th>
                     <th>Booking ID</th>
-                    {/* Add other table header as needed */}
                   </tr>
                 </thead>
                 <tbody>
-                  {payHistoryData
-                   
-                    .map((payment, index) => (
-                      <tr key={index}>
-                        <td>{payment.PaymentID}</td>
-                        <td>${payment.Value}</td>
-                        <td>{payment.PaymentType}</td>
-                        <td>{payment.PaymentDate}</td>
-                        <td>{payment.BookingID}</td>
-                        {/* Add other table data as needed */}
-                      </tr>
-                    ))}
+                  {filteredData.map((payment, index) => (
+                    <tr key={index}>
+                      <td>{payment.PaymentID}</td>
+                      <td>${payment.Value}</td>
+                      <td>{payment.PaymentType}</td>
+                      <td>{payment.PaymentDate}</td>
+                      <td>{payment.BookingID}</td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
